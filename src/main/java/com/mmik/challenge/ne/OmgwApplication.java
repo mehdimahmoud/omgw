620b4c94232099be1c003017217753a6f9e78b36
Mehdi MAHMOUD <mehdi.mahmoud@gmail.com>
2017-02-18 12:23:44 +0000

    First commit for project

---
+++ src/main/java/com/mmik/challenge/ne/OmgwApplication.java
@@ -0,0 +1,12 @@
+package com.mmik.challenge.ne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmgwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmgwApplication.class, args);
+	}
+}
